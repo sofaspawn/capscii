9255c574ac9c9c9bbe35c8c92e9c35b2ee681a10
sofaspawn <h.m1nus.work@gmail.com>
2024-12-24 13:39:38 +0530

    brightness_matrix calculation

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
+#! /usr/bin/env python3
+
 from PIL import Image
 import numpy as np
 
+ascii = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
+
 def get_brightness_matrix(pixel_matrix):
-    return [0.21*r + 0.72*g + 0.07*b for row in pixel_matrix for (r,g,b) in row]
+    brightness_matrix = np.zeros((pixel_matrix.shape[0], pixel_matrix.shape[1]))
+    for i in range(len(pixel_matrix)):
+        for j in range(len(pixel_matrix[i])):
+            r, g, b = pixel_matrix[i][j]
+            brightness_matrix[i][j] = 0.21*r + 0.72*g + 0.07*b
+    return brightness_matrix
 
 def main():
     img=Image.open("geto.jpeg")
+
     px_arr = np.array(img)
 
     brightness_matrix = get_brightness_matrix(px_arr)
