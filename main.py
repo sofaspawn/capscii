c90295e49ebe5dc5da4e03d00a84800ffabda348
sofaspawn <h.m1nus.work@gmail.com>
2024-12-24 11:29:26 +0530

    conflict resolved commit

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 #! /usr/bin/env python3
-
 from PIL import Image
-im = Image.open("geto.jpeg")
-print(im.format, im.size, im.mode)
+
+img=Image.open("geto.jpeg")
+px=img.load()
+(width,height)=img.size
+
+pixel_matrix=[[0 for _ in range(height)] for _ in range(width)]
+
+for x in range(width):
+    for y in range(height):
+        pixel_matrix[x][y]=(px[x,y])
+#print(pixel_matrix)
+
+brightness_matrix=[(r+g+b)/3 for row in pixel_matrix for (r,g,b) in row]
+print(brightness_matrix)
